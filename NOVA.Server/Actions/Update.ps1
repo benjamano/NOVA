f3aaa334bd4914f9f17d7d82652cc8cf7721fbb2
Ben Mercer <benmercer76@btinternet.com>
2024-12-27 23:30:36 +0000

    Adding update script

---
+++ NOVA.Server/Actions/Update.ps1
@@ -0,0 +1,17 @@
+# C# Backend
+echo "Building backend..."
+dotnet restore ../NOVAServer.csproj
+dotnet build --configuration Release ../NOVAServer.csproj
+dotnet publish ../NOVAServer.csproj -c Release -o /var/www/backend
+
+dotnet restore ../../nova.data/NOVAData.csproj
+dotnet build --configuration Release ../../nova.data/NOVAData.csproj
+dotnet publish ../../nova.data/NOVAData.csproj -c Release -o /var/www/backend
+
+
+# React Frontend
+echo "Building frontend..."
+cd ../../nova.client
+npm install
+npm run build
+cp -Recurse -Force ./dist /var/www/backend/wwwroot
